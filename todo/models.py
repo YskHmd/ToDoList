6557da085cb9a02b9024a43b1f7fdd0954097500
YskHmd <yusuke7075@gmail.com>
2024-09-04 13:42:32 +0900

    models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+#タイトルと内容のクラス
+class TodoModel(models.Model):
+    title = models.CharField(max_length = 100)
+    memo = models.TextField()
