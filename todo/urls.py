8240afeea57e312318aab57144e14c8de7c48bd7
YskHmd <yusuke7075@gmail.com>
2024-09-04 15:58:37 +0900

    htmlがうまく出ない

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import ToDoList
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', ToDoList.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from .models import TodoModel
 # Create your views here.
+
+class ToDoList(ListView):
+    template_name = 'list.html'
+    model = TodoModel
