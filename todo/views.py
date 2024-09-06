eade9195572253369a1611f6b54e6210a3efb49d
YskHmd <yusuke7075@gmail.com>
2024-09-06 15:53:42 +0900

    update

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import TodoModel
 from django.urls import reverse_lazy#url遷移の為
 # Create your views here.
@@ -22,3 +22,9 @@ class ToDoDelete(DeleteView):
     template_name = 'delete.html'
     model = TodoModel #どこに保存するか指定
     success_url = reverse_lazy('list')#削除後にurl遷移の為
+    
+class ToDoUpdate(UpdateView):
+    template_name = 'update.html'
+    model = TodoModel #どこに保存するか指定
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')#編集後にurl遷移の為
